9b3f996dc9dead37f0e621aa68723b33dabb7586
Milo-Shen <Milo-Shen@outlook.com>
2025-09-14 21:23:16 +0800

    feat: add point1.toString === point2.toString example

--- ES6/Class/class_1.js
+++ ES6/Class/class_1.js
@@ -19,19 +19,19 @@ console.log(Point.prototype.constructor === Point);
 
 const point = new Point(2, 3);
 console.log('point.toString(): ', point.toString()); // (2, 3)
-console.log(point.hasOwnProperty('x')); // true
-console.log(point.hasOwnProperty('y')); // true
-console.log(point.hasOwnProperty('toString')); // false
-console.log(Point.prototype.hasOwnProperty('toString')); // true
-console.log(point.__proto__.hasOwnProperty('toString')); // true
+console.log("point.hasOwnProperty('x'): ", point.hasOwnProperty('x')); // true
+console.log("point.hasOwnProperty('y'): ", point.hasOwnProperty('y')); // true
+console.log("point.hasOwnProperty('toString'): ", point.hasOwnProperty('toString')); // false
+console.log("Point.prototype.hasOwnProperty('toString'): ", Point.prototype.hasOwnProperty('toString')); // true
+console.log("point.__proto__.hasOwnProperty('toString'): ", point.__proto__.hasOwnProperty('toString')); // true
 
 // getPrototypeOf: get to prototype of the instance
 // in this example, the prototype of point is Point
-console.log(Object.getPrototypeOf(point).hasOwnProperty('toString'));
-console.log(Point.prototype.hasOwnProperty('toString'));
+console.log("Object.getPrototypeOf(point).hasOwnProperty('toString'): ", Object.getPrototypeOf(point).hasOwnProperty('toString'));
+console.log("Point.prototype.hasOwnProperty('toString'): ", Point.prototype.hasOwnProperty('toString'));
 
 // ownFunc is defined on the instance level
 const point1 = new Point(1, 2);
 const point2 = new Point(1, 2);
-console.log(point1.ownFunc === point2.ownFunc);
-console.log(point2.toString === point2.toString);
+console.log('point1.ownFunc === point2.ownFunc: ', point1.ownFunc === point2.ownFunc);
+console.log('point1.toString === point2.toString', point1.toString === point2.toString);
