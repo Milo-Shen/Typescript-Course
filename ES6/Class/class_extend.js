9c34fe1c3c971b5243ce892bd4aab55edcdbede0
Milo-Shen <Milo-Shen@outlook.com>
2025-10-05 16:25:43 +0800

    feat: add class extend example

--- ES6/Class/class_extend.js
+++ ES6/Class/class_extend.js
@@ -14,3 +14,21 @@ class Bar extends Foo {
 }
 
 new Bar();
+
+// In the subclass's constructor() method, the this keyword can only be used after calling super();
+// otherwise, an error will be thrown.
+// This is because the construction of a subclass instance must first complete the inheritance from the parent class, and only the super() method enables the subclass instance to inherit from the parent class.
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+class ColorPoint extends Point {
+  constructor(x, y, color) {
+    // ReferenceError: Must call super constructor in derived class before accessing 'this' or returning from derived constructor
+    // this.color = color;
+    super(x, y);
+    this.color = color; // 正确
+  }
+}
