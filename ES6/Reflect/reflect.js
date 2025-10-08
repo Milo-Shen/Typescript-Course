b5d6e013c2a30898542c46b7e71b4f0a154bc75a
Milo-Shen <Milo-Shen@outlook.com>
2025-10-08 10:47:03 +0800

    feat: add reflect example

--- ES6/Reflect/reflect.js
+++ ES6/Reflect/reflect.js
@@ -28,3 +28,14 @@ console.log('target.test : ', target.test);
 console.log("'assign' in Object : ", 'assign' in Object); // true
 // 新写法
 console.log("Reflect.has(Object, 'assign') : ", Reflect.has(Object, 'assign')); // true
+
+// （4）Reflect对象的方法与Proxy对象的方法一一对应，只要是Proxy对象的方法，就能在Reflect对象上找到对应的方法。这就让Proxy对象可以方便地调用对应的Reflect方法，完成默认行为，作为修改行为的基础。也就是说，不管Proxy怎么修改默认行为，你总可以在Reflect上获取默认行为。
+new Proxy(target, {
+  set: function (target, name, value, receiver) {
+    const success = Reflect.set(target, name, value, receiver);
+    if (success) {
+      console.log('property ' + name + ' on ' + target + ' set to ' + value);
+    }
+    return success;
+  },
+});
