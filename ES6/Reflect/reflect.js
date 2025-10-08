14311101846768cee5c4071e0033dccd6e626cec
Milo-Shen <Milo-Shen@outlook.com>
2025-10-08 10:31:57 +0800

    feat: add reflect example

--- ES6/Reflect/reflect.js
+++ ES6/Reflect/reflect.js
@@ -1,4 +1,6 @@
-// The Reflect object, like the Proxy object, is also a new API provided by ES6 for manipulating objects. The Reflect object has several design purposes.
+// Reflect对象与Proxy对象一样，也是 ES6 为了操作对象而提供的新 API。Reflect对象的设计目的有这样几个。
+// （1） 将Object对象的一些明显属于语言内部的方法（比如Object.defineProperty），放到Reflect对象上。现阶段，某些方法同时在Object和Reflect对象上部署，未来的新方法将只部署在Reflect对象上。也就是说，从Reflect对象上可以拿到语言内部的方法。
+// （2） 修改某些Object方法的返回结果，让其变得更合理。比如，Object.defineProperty(obj, name, desc)在无法定义属性时，会抛出一个错误，而Reflect.defineProperty(obj, name, desc)则会返回false。
 
 // Old
 const target = {};
