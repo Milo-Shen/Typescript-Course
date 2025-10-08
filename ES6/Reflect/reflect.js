2e027d36c4cfee0305087623f93c781b6cbe23b0
Milo-Shen <Milo-Shen@outlook.com>
2025-10-08 14:31:01 +0800

    feat: add reflect example

--- ES6/Reflect/reflect.js
+++ ES6/Reflect/reflect.js
@@ -207,6 +207,7 @@ Reflect.has(myObject4, 'foo'); // true
 
 // Reflect.deleteProperty(obj, name)
 // Reflect.deleteProperty 方法等同于 delete obj[name]，用于删除对象的属性。
+// 该方法返回一个布尔值。如果删除成功，或者被删除的属性不存在，返回 true；删除失败，被删除的属性依然存在，返回 false。
 const myObj = { foo: 'bar' };
 // 旧写法
 delete myObj.foo;
