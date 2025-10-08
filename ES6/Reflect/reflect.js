b28ddde9cb1a9661f07a3bc8a41325cc9c4b11bf
Milo-Shen <Milo-Shen@outlook.com>
2025-10-08 10:57:06 +0800

    feat: add reflect example

--- ES6/Reflect/reflect.js
+++ ES6/Reflect/reflect.js
@@ -30,6 +30,7 @@ console.log("'assign' in Object : ", 'assign' in Object); // true
 console.log("Reflect.has(Object, 'assign') : ", Reflect.has(Object, 'assign')); // true
 
 // （4）Reflect对象的方法与Proxy对象的方法一一对应，只要是Proxy对象的方法，就能在Reflect对象上找到对应的方法。这就让Proxy对象可以方便地调用对应的Reflect方法，完成默认行为，作为修改行为的基础。也就是说，不管Proxy怎么修改默认行为，你总可以在Reflect上获取默认行为。
+// 下面代码中，Proxy方法拦截target对象的属性赋值行为。它采用Reflect.set方法将值赋值给对象的属性，确保完成原有的行为，然后再部署额外的功能。
 new Proxy(target, {
   set: function (target, name, value, receiver) {
     const success = Reflect.set(target, name, value, receiver);
@@ -37,5 +38,22 @@ new Proxy(target, {
       console.log('property ' + name + ' on ' + target + ' set to ' + value);
     }
     return success;
+  },
+});
+
+// 下面是另一个例子。
+const obj = {};
+const loggedObj = new Proxy(obj, {
+  get(target, name) {
+    console.log('get', target, name);
+    return Reflect.get(target, name);
+  },
+  deleteProperty(target, name) {
+    console.log('delete' + name);
+    return Reflect.deleteProperty(target, name);
+  },
+  has(target, name) {
+    console.log('has' + name);
+    return Reflect.has(target, name);
   },
 });
