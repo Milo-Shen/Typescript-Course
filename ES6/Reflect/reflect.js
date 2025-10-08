dbdc10c748ad0f1c25d3af8fcfcc5e73306db363
Milo-Shen <Milo-Shen@outlook.com>
2025-10-08 14:38:58 +0800

    feat: add reflect example

--- ES6/Reflect/reflect.js
+++ ES6/Reflect/reflect.js
@@ -218,3 +218,10 @@ console.log('myObj.foo', myObj.foo); // undefined
 
 // Reflect.construct(target, args)
 // Reflect.construct 方法等同于 new target(...args)，这提供了一种不使用 new，来调用构造函数的方法。
+function Greeting(name) {
+  this.name = name;
+}
+// new 的写法
+let instance = new Greeting('张三');
+// Reflect.construct 的写法
+instance = Reflect.construct(Greeting, ['张三']);
