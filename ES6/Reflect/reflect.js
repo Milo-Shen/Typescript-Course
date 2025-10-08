9c38171aef3884d7b65011b72945b65f09f7597e
Milo-Shen <Milo-Shen@outlook.com>
2025-10-08 10:30:36 +0800

    feat: add reflect example

--- ES6/Reflect/reflect.js
+++ ES6/Reflect/reflect.js
@@ -1,1 +1,22 @@
 // The Reflect object, like the Proxy object, is also a new API provided by ES6 for manipulating objects. The Reflect object has several design purposes.
+
+// Old
+const target = {};
+const property = 'test';
+const attributes = {
+  value: 'test',
+};
+try {
+  Object.defineProperty(target, property, attributes);
+  // success
+} catch (e) {
+  // failure
+}
+
+// New
+if (Reflect.defineProperty(target, property, attributes)) {
+  // success
+} else {
+  // failure
+}
+console.log('target.test : ', target.test);
