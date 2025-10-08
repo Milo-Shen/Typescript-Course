ba0f41e0f52c3435d9667add3bfe8ba804be8d14
Milo-Shen <Milo-Shen@outlook.com>
2025-10-08 11:02:54 +0800

    feat: add reflect example

--- ES6/Reflect/reflect.js
+++ ES6/Reflect/reflect.js
@@ -65,3 +65,19 @@ const loggedObj = new Proxy(obj, {
 Function.prototype.apply.call(Math.floor, undefined, [1.75]); // 1
 // 新写法
 Reflect.apply(Math.floor, undefined, [1.75]); // 1
+
+// 静态方法
+// Reflect对象一共有 13 个静态方法。
+// - Reflect.apply(target, thisArg, args)
+// - Reflect.construct(target, args)
+// - Reflect.get(target, name, receiver)
+// - Reflect.set(target, name, value, receiver)
+// - Reflect.defineProperty(target, name, desc)
+// - Reflect.deleteProperty(target, name)
+// - Reflect.has(target, name)
+// - Reflect.ownKeys(target)
+// - Reflect.isExtensible(target)
+// - Reflect.preventExtensions(target)
+// - Reflect.getOwnPropertyDescriptor(target, name)
+// - Reflect.getPrototypeOf(target)
+// - Reflect.setPrototypeOf(target, prototype)
