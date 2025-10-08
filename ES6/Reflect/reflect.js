c3f81f6f6a48d62fa44d302532b9233e86659266
Milo-Shen <Milo-Shen@outlook.com>
2025-10-08 10:44:48 +0800

    feat: add reflect example

--- ES6/Reflect/reflect.js
+++ ES6/Reflect/reflect.js
@@ -24,3 +24,7 @@ if (Reflect.defineProperty(target, property, attributes)) {
 console.log('target.test : ', target.test);
 
 // (3） 让Object操作都变成函数行为。某些Object操作是命令式，比如name in obj和delete obj[name]，而Reflect.has(obj, name)和Reflect.deleteProperty(obj, name)让它们变成了函数行为。
+// 老写法
+console.log("'assign' in Object : ", 'assign' in Object); // true
+// 新写法
+console.log("Reflect.has(Object, 'assign') : ", Reflect.has(Object, 'assign')); // true
