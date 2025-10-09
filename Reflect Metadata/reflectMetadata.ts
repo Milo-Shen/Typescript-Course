abf64bf761252eb4eabe5850e378a5027fb91b47
Milo-Shen <Milo-Shen@outlook.com>
2025-10-09 17:08:06 +0800

    feat: add reflect metadata

--- Reflect Metadata/reflectMetadata.ts
+++ Reflect Metadata/reflectMetadata.ts
@@ -11,7 +11,12 @@ class MyClass {
   }
 }
 
+// define metadata on instance
 const instance = Reflect.construct(MyClass, ['MyClassParam']);
 Reflect.defineMetadata('key1', 'value1', instance, 'myProperty');
 const hasMetadata = Reflect.hasMetadata('key1', instance, 'myProperty');
 console.log('myProperty on instance has Metadata key1: ', hasMetadata);
+
+// get metadata from instance
+const metadataValue = Reflect.getMetadata('key1', instance, 'myProperty');
+console.log('key1 metadata value on instance: ', metadataValue);
