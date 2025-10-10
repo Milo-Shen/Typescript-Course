c6e344be898b10c43131c7f9593965ce0bf25835
Milo-Shen <Milo-Shen@outlook.com>
2025-10-10 10:14:33 +0800

    feat: add decorator example

--- Decorators/Old Syntax/decorator.ts
+++ Decorators/Old Syntax/decorator.ts
@@ -1,1 +1,12 @@
 // 1. experimentalDecorators 编译选项
+// $ tsc --target ES5 --experimentalDecorators
+// 此外，还有另外一个编译选项--emitDecoratorMetadata，用来产生一些装饰器的元数据，供其他工具或某些模块（比如 reflect-metadata ）使用。
+// 这两个编译选项可以在命令行设置，也可以在tsconfig.json文件里面进行设置。
+
+// {
+//   "compilerOptions": {
+//   "target": "ES6",
+//     "experimentalDecorators": true,
+//     "emitDecoratorMetadata": true
+// }
+// }
