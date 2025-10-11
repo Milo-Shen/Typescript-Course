9f429a65a6b48bc790986da948961cc808093e9b
Milo-Shen <Milo-Shen@outlook.com>
2025-10-11 22:12:36 +0800

    feat: add decorator example

--- src/Decorators/NewSyntax/decorator.ts
+++ src/Decorators/NewSyntax/decorator.ts
@@ -5,3 +5,10 @@
 // （3）这个函数接受所修饰对象的一些相关值作为参数。
 // （4）这个函数要么不返回值，要么返回一个新对象取代所修饰的目标对象。
 // 举例来说，有一个函数Injectable()当作装饰器使用，那么需要写成@Injectable，然后放在某个类的前面。
+function simpleDecorator(value: any, context: any) {
+  console.log(`hi, this is ${context.kind} ${context.name}`);
+  return value;
+}
+
+@simpleDecorator
+class A {} // "hi, this is class A"
