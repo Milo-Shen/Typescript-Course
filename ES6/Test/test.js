9ed403f11fe38849a10e64eb2ddf12b15f6b5beb
Milo-Shen <Milo-Shen@outlook.com>
2025-10-11 10:59:38 +0800

    feat: add decorator example

--- ES6/Test/test.js
+++ ES6/Test/test.js
@@ -14,5 +14,6 @@ const test = new Test();
 test.app = 1;
 console.log('test.a : ', test.a);
 console.log('Reflect.ownKeys(Test.prototype) : ', Reflect.ownKeys(Test.prototype));
+console.log('Reflect.ownKeys(test) : ', Reflect.ownKeys(test));
 console.log('test.app : ', test.app);
 console.log("Reflect.getOwnPropertyDescriptor(test, 'app') : ", Reflect.getOwnPropertyDescriptor(test, 'app'));
