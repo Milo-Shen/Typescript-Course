dbcf23b529a18922c0706393d69eeec89f1b8a2d
Milo-Shen <Milo-Shen@outlook.com>
2025-10-11 09:30:06 +0800

    feat: add decorator example

--- ES6/Test/test.js
+++ ES6/Test/test.js
@@ -1,6 +1,7 @@
 class Test {
   a = 1;
   b = 2;
+  app = 4;
   set app(val) {
     console.log('set: ', val);
     return val;
@@ -10,12 +11,3 @@ class Test {
 const test = new Test();
 test.app = 1;
 console.log(Object.getOwnPropertyNames(Test.prototype));
-
-class Test2 {}
-Reflect.defineProperty(Test2.prototype, 'app', {
-  get() {
-    return 1;
-  },
-});
-console.log(new Test2().app);
-console.log(Object.getOwnPropertyNames(Test2.prototype));
