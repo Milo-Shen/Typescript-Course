1b19b34a3e4801f07ec709cc5bb5c9beffc22067
Milo-Shen <Milo-Shen@outlook.com>
2025-10-12 16:23:37 +0800

    feat: add decorator example

--- src/Decorators/NewSyntax/decorator.ts
+++ src/Decorators/NewSyntax/decorator.ts
@@ -305,7 +305,7 @@ function bound(originalMethod: any, context: ClassMethodDecoratorContext) {
   if (context.private) {
     throw new Error(`不能绑定私有方法 ${methodName as string}`);
   }
-  context.addInitializer(function () {
+  context.addInitializer(function (this: any) {
     this[methodName] = this[methodName].bind(this);
   });
 }
